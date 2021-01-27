d40e083e4f0c22210c0b62420a49658cefc41c80
Satyadeva97 <satyadeva.ankey@gmail.com>
2021-01-27 15:35:09 -0500

    Naive solution

---
+++ LeetCode/MinimumNumberOfPlatformsRequired.java
@@ -0,0 +1,22 @@
+public class MinimumNumberOfPlatformsRequired {
+  public static void main(String[] args) {
+    int arrival[] = new int[]{ 900, 940, 950, 1100, 1500, 1800 };
+    int departure[] = new int[]{ 910, 1200, 1120, 1130, 1900, 2000 };
+    System.out.println(calculateNumberOfStops(arrival, departure));
+  }
+
+  private static int calculateNumberOfStops(int[] arrival, int[] departure) {
+    int result = 0;
+    int count;
+    for (int i = 0; i < arrival.length; i++) {
+      count = 0;
+      for (int j = i; j < arrival.length; j++) {
+        if (arrival[j] < departure[i] && arrival[j] >= arrival[i]) {
+          count++;
+        }
+      }
+      result = Math.max(result, count);
+    }
+    return result;
+  }
+}
