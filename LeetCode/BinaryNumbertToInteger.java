ad157a324853fd20d5d7a1700aa11a279131cf74
Satyadeva97 <satyadeva.ankey@gmail.com>
2021-02-07 22:10:14 -0500

    leetcode 1290

---
+++ LeetCode/BinaryNumbertToInteger.java
@@ -0,0 +1,15 @@
+public class BinaryNumbertToInteger {
+  public int getDecimalValue(ListNode head) {
+    List<Integer> list = new ArrayList<>();
+    while (head != null) {
+      list.add(head.val);
+      head =head.next;
+    }
+
+    int sum = 0;
+    for (int j = list.size() - 1; j >= 0; j--) {
+      sum += Math.pow(2, list.size() - (j + 1)) * list.get(j);
+    }
+    return sum;
+  }
+}
